e32aba526fb21b98f874d8303c5c9cf3e15e9968
Rand Black <braniganrobot@me.com>
2019-05-05 19:46:44 -0700

    heroku files

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "us-cdbr-iron-east-02.cleardb.net",
   port: 3306,
-  user: 'webuser',
-  password: 'UCR',
-  database: "burgers_db"
+  user: 'ba2abb1a76a0cd',
+  password: 'ab9c6df8',
+  database: "heroku_9657cff2c406291"
 });
 
 module.exports = connection;
